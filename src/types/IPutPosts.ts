c3690180c3c46b85b903929a7ff46cb89129295e
chane81 <chane81@naver.com>
2021-10-27 00:41:49 +0900

    Add type

---
+++ src/types/IPutPosts.ts
@@ -0,0 +1,13 @@
+export interface IReqPutPosts {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+export interface IResPutPosts {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
